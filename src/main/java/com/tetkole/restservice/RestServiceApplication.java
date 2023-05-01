855d885f28e779862fbb77cbb559e5945c820280
Rémi Chavance <60784896+RemiChavance@users.noreply.github.com>
2023-05-01 19:17:21 +0200

    [ + ] creation of admin at start

--- src/main/java/com/tetkole/restservice/RestServiceApplication.java
+++ src/main/java/com/tetkole/restservice/RestServiceApplication.java
@@ -1,12 +1,38 @@
 package com.tetkole.restservice;
 
+import com.tetkole.restservice.models.Role;
+import com.tetkole.restservice.models.User;
+import com.tetkole.restservice.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class RestServiceApplication {
+@RequiredArgsConstructor
+public class RestServiceApplication implements CommandLineRunner {
+
+	private final UserRepository userRepository;
+	private final PasswordEncoder passwordEncoder;
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(RestServiceApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) {
+		if (userRepository.existsByEmail("admin@mail.com")) return;
+
+		// create a default user
+		User user = new User();
+		user.setFirstname("admin");
+		user.setLastname("admin");
+		user.setPassword(passwordEncoder.encode("admin"));
+		user.setEmail("admin@mail.com");
+		user.setRole(Role.ADMIN);
+		userRepository.save(user);
 	}
 
 }
